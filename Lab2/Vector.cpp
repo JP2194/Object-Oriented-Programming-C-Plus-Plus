bff571fa052febf9a74e74d2238ceb23ef2909d7
JP2194 <joshuapaul@cpp.edu>
2025-02-14 21:36:48 -0800

    Lab 2
    Code for lab 2 Array.cpp and Vector.cpp

---
+++ Lab2/Vector.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * @brief prints the elements in the vector and their memory locations
+ * 
+ * @param v - vector of intergers
+ * 
+**/
+
+void printMemVec(vector<int> v, int size)
+{
+    printf("Vector - Each int is worth %lu bytes\n", sizeof(v[0]));
+    for (int i = 0; i < size; i++)
+    {
+        printf("Value:%i at Memory Location:%p\n", v[i], &v[i]);
+    }
+}
+
+void incVecBy10(vector<int> &v,int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        v[i] += 10;
+    }
+}
+
+int main()
+{
+    const int SIZE = 5;
+    vector<int> vec(SIZE);
+    for (int i = 0; i<SIZE; i++)
+    {
+        vec[i] = i + 100;
+    }
+
+    printf("Before Increment-------\n");
+    printMemVec(vec,SIZE);
+    incVecBy10(vec,SIZE);
+
+    printf("After Increment-------\n");
+    printMemVec(vec,SIZE);
+    vec.pop_back();
+
+
+    printf("After Pop-------\n");
+    printMemVec(vec,SIZE);
+    vec.push_back(101);
+    vec.push_back(102);
+
+    printf("After Push-------\n");
+    printMemVec(vec,SIZE);
+
+    return 0;
+}

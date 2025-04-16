504b35c0bb6535219aa39ef2a12d86d86259ec38
JP2194 <joshuapaul@cpp.edu>
2025-04-15 22:57:39 -0700

    Lab 5 code
    inheritance and polymorphism

---
+++ Lab5/Pokemon.h
@@ -0,0 +1,30 @@
+#ifndef POKEMON_H
+#define POKEMON_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Pokemon
+{
+    public:
+        //----Constructors----
+        Pokemon();
+        Pokemon(string name, int hp, int att, int def, vector<string> type);
+
+        //----Mutators----
+        virtual void speak();
+        virtual void printStats();
+
+    //----Accessor Functions
+
+    protected:
+        string name;
+        int hp;
+        int attack;
+        int defense;
+        vector<string> type;
+
+};
+
+#endif
